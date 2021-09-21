c832b9a28a5515f92c9203df3853d3719ce27d97
Your Name <you@example.com>
2021-09-21 18:08:05 +0500

    test

--- utils/write_google.py
+++ utils/write_google.py
@@ -8,7 +8,7 @@ from googleapiclient.discovery import build
 def write_google(lname, fname, mname, phone):
     SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    SERVICE_ACCOUNT_FILE = os.path.join(BASE_DIR, 'credentials.json')
+    SERVICE_ACCOUNT_FILE = os.path.join(BASE_DIR, 'credentials1.json')
     print(SERVICE_ACCOUNT_FILE)
     credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE,
                                                                         scopes=SCOPES)
@@ -20,7 +20,7 @@ def write_google(lname, fname, mname, phone):
         "majorDimension": "ROWS",
         "values": lst
     }
-    spreadsheetId = "1G7Qb7bJD_tFnp4gy4vzxjewoJibz8Nhwy82LbE6KerQ"
+    spreadsheetId = "1xcqARLS0TwJPDz-jlyZ7fk3lq--9L6WaQpnMybWp6oQ"
     range_sheets = "list!A:D"
     service.spreadsheets().values().append(
         spreadsheetId=spreadsheetId,
