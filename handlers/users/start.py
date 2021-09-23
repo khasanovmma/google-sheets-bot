66fc4cf911ee671811b3f3e6c626d61cb8f0165e
Miraziz <none>
2021-09-23 20:20:34 +0500

    test

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,6 +1,7 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
+from data.config import ADMINS
 from filters import IsPrivate
 from loader import dp
 from states.answer import Answer
@@ -8,10 +9,13 @@ from states.answer import Answer
 
 @dp.message_handler(IsPrivate(), CommandStart())
 async def bot_start(message: types.Message):
-    await message.delete()
-    await message.answer(f"""🇺🇿 Assalomu aleykum, ro'yxatdan o'tishni boshlaymiz.
+    if str(message.chat.id) not in ADMINS:
+            await message.delete()
+            await message.answer(f"""🇺🇿 Assalomu aleykum, ro'yxatdan o'tishni boshlaymiz.
 Familiyangizni kiriting:\n
 🇷🇺 Здравствуйте, начнем регистрацию.
 Введите фамилию:
 """)
-    await Answer.last_name.set()
+            await Answer.last_name.set()
+    else:
+        await message.answer("Hello admin")
--- utils/write_google.py
+++ utils/write_google.py
@@ -8,7 +8,7 @@ from googleapiclient.discovery import build
 def write_google(lname, fname, mname, phone):
     SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    SERVICE_ACCOUNT_FILE = os.path.join(BASE_DIR, 'credentials1.json')
+    SERVICE_ACCOUNT_FILE = os.path.join(BASE_DIR, 'credentials.json')
     print(SERVICE_ACCOUNT_FILE)
     credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE,
                                                                         scopes=SCOPES)
@@ -20,8 +20,8 @@ def write_google(lname, fname, mname, phone):
         "majorDimension": "ROWS",
         "values": lst
     }
-    spreadsheetId = "1xcqARLS0TwJPDz-jlyZ7fk3lq--9L6WaQpnMybWp6oQ"
-    range_sheets = "Info!A:D"
+    spreadsheetId = "1G7Qb7bJD_tFnp4gy4vzxjewoJibz8Nhwy82LbE6KerQ"
+    range_sheets = "list!A:D"
     service.spreadsheets().values().append(
         spreadsheetId=spreadsheetId,
         range=range_sheets,
